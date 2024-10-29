5c1b25c88ad4d04c70626ac5aab2d594958170d1
mi3765 <chips1225@outlook.jp>
2024-10-30 00:28:00 +0900

    chore: .vercelignoreファイルの作成と設定

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -1,10 +1,7 @@
 import { createClient } from "microcms-js-sdk";
 
-// const serviceDomain = process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN;
-// const apiKey = process.env.NEXT_PUBLIC_MICROCMS_API_KEY;
-
-const serviceDomain = "q2xe9iop9c";
-const apiKey = "xTUPx1YgjydcdqYfOcBp0RmVg9mQ7gupxSxG";
+const serviceDomain = process.env.MICROCMS_SERVICE_DOMAIN;
+const apiKey = process.env.MICROCMS_API_KEY;
 
 if (!serviceDomain || !apiKey) {
 	throw new Error("Missing microCMS service domain or API key");
