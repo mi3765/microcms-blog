709e65548c0ee59bed3958d09c32d921ab960cef
mi3765 <chips1225@outlook.jp>
2024-10-31 00:56:30 +0900

    fix: 環境変数の設定を修正し、microCMSのAPIキーとサービスドメインを正しく読み取るように修正

--- libs/microcms.ts
+++ libs/microcms.ts
@@ -1,7 +1,10 @@
 import { createClient } from "microcms-js-sdk";
 
-const serviceDomain = process.env.MICROCMS_SERVICE_DOMAIN;
-const apiKey = process.env.MICROCMS_API_KEY;
+const serviceDomain = process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN;
+const apiKey = process.env.NEXT_PUBLIC_MICROCMS_API_KEY;
+
+console.log("Service Domain:", serviceDomain);
+console.log("API Key:", apiKey);
 
 if (!serviceDomain || !apiKey) {
 	throw new Error("Missing microCMS service domain or API key");
